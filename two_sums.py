10a728d3a9d1b5e7caf964f54e10e99b03bfd194
Emanuel Calderon <ecalde11@jh.edu>
2022-12-13 17:22:56 -0500

    solution 1

--- two_sums.py
+++ two_sums.py
@@ -9,11 +9,21 @@ class Solution(object):
         starter_integer = 0
         for integer in nums:
             value = starter_integer + integer
-            if value == target:
-                output_list.append(target)
+            # print("starter: " + str(starter_integer))
+            # print("integer: " + str(integer))
+            # print("value: " + str(value))
+            # print("")
+            if value == target and starter_integer != 0:
+                output_list.append(nums.index(starter_integer))
+                output_list.append(nums.index(integer))
+                starter_integer = integer
             else:
+                starter_integer = integer
                 continue
-            starter_integer = integer
         return output_list
 
-        
+nums = [2,7,11,15]
+target = 9
+solution = []
+
+print(Solution.twoSum(solution,nums, target))
